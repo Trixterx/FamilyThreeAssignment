c49d7dbb69fc69d2bcd08b12928bca8e55a47966
Dennis Lindquist <dennislindquist@gmail.com>
2021-02-15 12:41:19 +0100

    jklklj

--- FamilyThreeAssignment/CRUD.cs
+++ FamilyThreeAssignment/CRUD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace FamilyThreeAssignment
@@ -9,13 +10,34 @@ namespace FamilyThreeAssignment
         public string DatabaseName { get; set; } = "FamilyThree";
         public int MaxRows { get; set; } = 10;
         public string OrderBy { get; set; } = "lastName";
+        internal string ConnectionString { get; set; } = @"Data Source=.\SQLExpress;Integrated Security=true;database=Persons";
+
+
         public void Create(Person person)
         {
-
+            var connString = string.Format(ConnectionString, DatabaseName);
+            var cnn = new SqlConnection(connString);
+            cnn.Open();
+            var sql = "INSERT INTO People(firstname, lastname, mother, father) VALUES (@firstname, @lastname, @mother, @father)";
+            var command = new SqlCommand(sql, cnn);
+            command.Parameters.AddWithValue("@firstname", person.FirstName);
+            command.Parameters.AddWithValue("@lastname", person.LastName);
+            command.Parameters.AddWithValue("@mother", person.Mother);
+            command.Parameters.AddWithValue("@father", person.Father);
+            command.ExecuteNonQuery();
         }
         public void Delete(Person person)
         {
-
+            var connString = string.Format(ConnectionString, DatabaseName);
+            var cnn = new SqlConnection(connString);
+            cnn.Open();
+            var sql = "DELETE FROM People(firstname, lastname, mother, father) VALUES (@firstname, @lastname, @mother, @father)";
+            var command = new SqlCommand(sql, cnn);
+            command.Parameters.AddWithValue("@firstname", person.FirstName);
+            command.Parameters.AddWithValue("@lastname", person.LastName);
+            command.Parameters.AddWithValue("@mother", person.Mother);
+            command.Parameters.AddWithValue("@father", person.Father);
+            command.ExecuteNonQuery();
         }
         public bool DoesPersonExist(string name)
         {
